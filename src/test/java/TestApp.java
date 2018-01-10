39be6720087b4a250f4ef8cfd7494891ee2bbd2d
chikna <chikna88@gmail.com>
2018-01-10 13:25:42 -0600

    Added test file

---
+++ src/test/java/TestApp.java
@@ -0,0 +1,15 @@
+package com.maxo;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestApp {
+    @Test
+    public void testhello() {
+       HelloWorld hello = new HelloWorld();
+       double amount = account.debit(5
+	   String name="Hello";
+       Assert.assertEquals("Hello", name);
+    }
+}
